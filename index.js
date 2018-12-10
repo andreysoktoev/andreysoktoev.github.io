d2d1fd294086754de48746cbcab0ab9f978da63e
Andrey <andrey.soktoev@gmail.com>
2018-12-10 22:00:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 window.onload = function () {
   document.documentElement.style.display = 'inline';
   document.documentElement.style.opacity = '1';
+  document.body.style.minHeight = window.innerHeight + 'px';
 }
-// Размеры окна
-let scrollHeight = Math.max(
-  document.body.scrollHeight, document.documentElement.scrollHeight,
-  document.body.offsetHeight, document.documentElement.offsetHeight,
-  document.body.clientHeight, document.documentElement.clientHeight
-);
+
 function heightChecker() {
+  let scrollHeight = Math.max(
+    // document.body.scrollHeight, document.documentElement.scrollHeight,
+    // document.body.offsetHeight, document.documentElement.offsetHeight,
+    // document.body.clientHeight, document.documentElement.clientHeight
+    window.innerHeight
+  );
   alert('Высота с учетом прокрутки: ' + scrollHeight);
 }
