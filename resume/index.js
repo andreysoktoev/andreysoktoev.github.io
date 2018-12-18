279b4e2f2f46401eb46b0cfbd88344fff98e87c4
Andrey <andrey.soktoev@gmail.com>
2018-12-19 02:21:35 +0900

    nav

--- resume/index.js
+++ resume/index.js
@@ -2,7 +2,12 @@ window.onload = function () {
   document.documentElement.style.opacity = '1';
 }
 function menu() {
-  document.querySelectorAll('.nav_item').forEach(i => i.classList.add('hidden'));
-  event.target.style.background = 'rgba(255, 255, 255, .95)';
-  event.target.style.color = '#282828';
+  if (event.target.dataset.hidden == 1) {
+    document.querySelectorAll('.nav_item').forEach(i => i.classList.add('open'));
+    event.target.dataset.hidden = '0';
+  }
+  else {
+    document.querySelectorAll('.nav_item').forEach(i => i.classList.remove('open'));
+    event.target.dataset.hidden = '1';
+  }
 }
